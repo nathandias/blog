da6bf98271aa12ad2b5e8fc0f8107840dde10ee9
Nathan Dias <nathan.dias@gmail.com>
2021-07-14 23:28:57 -0700

    after section 10 / ep 54

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -17,6 +18,11 @@ class Post extends Model
     // {
     //     return 'slug';
     // }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     public function category()
     {
--- database/migrations/2021_07_15_054238_create_comments_table.php
+++ database/migrations/2021_07_15_054238_create_comments_table.php
@@ -1,10 +1,11 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +14,20 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id')->contrained()->cascadeOnDelete();
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
+
+            // $table->unsignedBigInteger('post_id');
+            // $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
+
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('post_id')->references('id')->on('users')->cascadeOnDelete();
+
+
         });
     }
 
@@ -33,6 +38,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }
