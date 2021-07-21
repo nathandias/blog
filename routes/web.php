f310f6b1771731953ce0420eacb2aff655c34a02
Nathan Dias <nathan.dias@gmail.com>
2021-07-21 13:28:01 -0700

    after ep 61

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Services\Newsletter;
+use MailchimpMarketing\ApiClient;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NewsletterController;
+use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\PostCommentsController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
@@ -20,3 +24,8 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store'])->middleware('auth');
 
 // index, show, create, store, edit, update, destroy
+
+
+Route::post('newsletter', NewsletterController::class);
+
+
